d00b1f360e1a1da75aae29c9cc98bb2a87ab6d7d
ihwan <ihwanptr@ymail.com>
2022-10-24 14:34:49 +0700

    first commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Ihwan Koding");
+
+
+        for (int i = 1; i <= 10; i++) {
+            if (i % 2 !=0) {
+                System.out.println("Angka "+ i + " adalah Ganjil");
+            }
+            else{
+                System.out.println("Angka "+ i +" adalah Genap");
+            }
+          }
+    }
+}
